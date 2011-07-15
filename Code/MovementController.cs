09f9151ec22a08789adda8ed0c76d1d3e08d6ff1
Mark Hanford <mark.hanford@intellectuk.org>
2011-07-15 13:33:43 +0100

    Testing out a Netduino version

---
+++ Code/MovementController.cs
@@ -0,0 +1,56 @@
+using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using System.Threading;
+
+namespace Bot
+{
+    class MovementController
+    {
+        private Wheel LeftWheel;
+        private Wheel RightWheel;
+        private DateTime LastUpdate;
+
+        public MovementController(Cpu.Pin PinLA, Cpu.Pin PinLB, Cpu.Pin PinLP, Cpu.Pin PinRA, Cpu.Pin PinRB, Cpu.Pin PinRP)
+        {
+            LeftWheel = new Wheel(PinLA, PinLB, PinLP);
+            RightWheel = new Wheel(PinRA, PinRB, PinRP);
+        }
+
+        public void wakeup()
+        {
+            LeftWheel.setSpeed(10);
+            RightWheel.setSpeed(10);
+            LeftWheel.update();
+            RightWheel.update();
+            Thread.Sleep(200);
+
+            LeftWheel.setSpeed(-10);
+            RightWheel.setSpeed(-10);
+            LeftWheel.update();
+            RightWheel.update();
+            Thread.Sleep(200);
+
+            LeftWheel.setSpeed(0);
+            RightWheel.setSpeed(0);
+            LeftWheel.update();
+            RightWheel.update();
+        }
+
+        public void update()
+        {
+            LeftWheel.update();
+            RightWheel.update();
+            LastUpdate = DateTime.Now;
+        }
+
+        public void stop()
+        {
+            LeftWheel.setSpeed(0);
+            RightWheel.setSpeed(0);
+        }
+
+    }
+}
---
+++ Code/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace Bot
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            MovementController movement = new MovementController(0, 0, 0, 0, 0, 0);
+
+            movement.wakeup();
+
+            while (true)
+            {
+                movement.update();
+            }
+        }
+
+    }
+}
